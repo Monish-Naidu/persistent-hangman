8ce919602c202bb1872b8d6177c25d6803fa20ef
Monish-Naidu <31556176+Monish-Naidu@users.noreply.github.com>
2018-10-21 11:51:28 -0700

    brought in code from old hangman game

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+word = "vivek"
+
+
+word_database = {
+    "guessed": [],
+    "known": ["", "", "", "", ""],
+    "message": ""
+}
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/start')
+def start():
+    return "starting a hangman game"
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/guess')
+def guess():
+    guess = request.args.get('letter')
+    if len(guess) != 1:
+        word_database["message"] = "The guess was not one letter"
+        return jsonify(word_database)
+    if guess not in "abcdefghijklmnopqrstuvwxyz":
+        word_database["message"] = "The letter " + guess + " is not part of the alphabet"
+        return jsonify(word_database)
+    if guess in word_database["guessed"]:
+        word_database["message"] = "You have already guessed " + guess + " try again"
+    if guess in word:
+        for i in range(0, len(word)):
+            if guess == word[i]:
+                word_database["known"][i] = guess
+        word_database["guessed"].append(guess)
+        word_database["message"] = "Good job playa! the letter " + guess + " is in the word!"
+        return jsonify(word_database)
+    if guess not in word:
+        word_database["message"] = guess + " is not in the word"
+        word_database["guessed"].append(guess)
+        return jsonify(word_database)
+
+
+
